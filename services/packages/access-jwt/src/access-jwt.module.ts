c4ee4669566ff264595a943aa77a84c8ebf7cac9
Cyrus <cyrus.kuznetsov@gmail.com>
2023-06-16 13:49:14 +0500

    fix: fix config module loading

--- services/packages/access-jwt/src/access-jwt.module.ts
+++ services/packages/access-jwt/src/access-jwt.module.ts
@@ -1,23 +1,35 @@
 import { Reflector } from '@nestjs/core';
 import { Module, DynamicModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import {
+  ConfigModule,
+  ConfigModuleOptions,
+  ConfigService,
+} from '@nestjs/config';
+import { DatabaseModule } from '@collection.io/prisma';
 
 import { VerifyTokenService } from './verify-token.service';
 import { CreateTokenService } from './create-token.service';
 import { UserInfoService } from './user-info.service';
 import { AccessStrategy } from './access.strategy';
-import { DatabaseModule } from '@collection.io/prisma';
 
 @Module({})
 export class AccessJwtModule {
-  static forRoot(configSecretKey: string): DynamicModule {
+  static forRoot(
+    configSecretKey: string,
+    configOptions: ConfigModuleOptions = {},
+  ): DynamicModule {
     return {
       module: AccessJwtModule,
       imports: [
         DatabaseModule,
-        JwtModule.register({
-          secret: process.env[configSecretKey],
-          signOptions: { expiresIn: '10m' },
+        JwtModule.registerAsync({
+          imports: [ConfigModule.forRoot(configOptions)],
+          inject: [ConfigService],
+          useFactory: (cfg: ConfigService) => ({
+            secret: cfg.get<string>(configSecretKey),
+            signOptions: { expiresIn: '10m' },
+          }),
         }),
       ],
       providers: [
