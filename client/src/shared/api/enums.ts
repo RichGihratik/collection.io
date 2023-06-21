aca61a10ba94037b64e2192d150a2d88e16b38d7
Cyrus <cyrus.kuznetsov@gmail.com>
2023-06-21 16:22:47 +0500

    feat: add request statuses

---
+++ client/src/shared/api/enums.ts
@@ -0,0 +1,5 @@
+export enum QueryStatus {
+  Unauthorised = 401,
+  Forbidden = 403,
+  BadRequest = 400,
+}
--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,3 +1,4 @@
 export { del, post, patch, get, type QueryOptions, getResInfo } from './queries';
 export { fetchWithToken } from './include-token';
 export { queryClient } from './query-client';
+export { QueryStatus } from './enums';
